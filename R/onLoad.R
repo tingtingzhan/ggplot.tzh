420463f776f4027ac3eecba336c36a42f7b5be03
Tingting Zhan <tingtingzhan@gmail.com>
2026-03-04 01:04:39 -0500

    tzh

--- R/onLoad.R
+++ R/onLoad.R
@@ -1,18 +1,15 @@
 
 
-# @importFrom parallel detectCores
 .onLoad <- function(libname, pkgname) {
   
-  Sys.setenv(
+  options(
     
-    '_R_CHECK_LIMIT_CORES_' = 'false',
-    # otherwise ?parallel:::.check_ncores causes error when ?devtools::check
+    # unicode support 
+    # MUST as of macOS, R 4.5.1
+    bitmapType = 'cairo' 
+    # must ship with package to alter user's options
     
-    '_R_CHECK_SYSTEM_CLOCK_' = 0
-    # https://stackoverflow.com/questions/63613301/r-cmd-check-note-unable-to-verify-current-time
-    # to disable "checking for future file timestamps" in R Check
-    
-  ) 
+  )  
   
 }
 
